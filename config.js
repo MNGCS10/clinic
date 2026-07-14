8a7eef1974df5f28f4961a0473674393fefbc3df
MNGCS10 <mng.cs10@gmail.com>
2026-07-14 18:01:31 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+// config.js — Supabase config กลาง ใช้ร่วมทุกหน้า
+const SUPABASE_URL = "https://yexrmeidwedazbiaiazy.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlleHJtZWlkd2VkYXpiaWFpYXp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM5NTUzMjEsImV4cCI6MjA5OTUzMTMyMX0.mABokSstxoizRDKK_UFCL6Zd52AqxfjSolKglWxokNk";
